42b31ac9c41832ac16dfa2d26df0153637844b3a
HoqueTouhidul97 <67569444+HoqueTouhidul97@users.noreply.github.com>
2020-08-09 02:54:09 +0600

    done practice

---
+++ arraySplit.js
@@ -0,0 +1,7 @@
+const nums = [1,2,3,4,5,6,7,8,9];
+const part = nums.slice(2,6);
+console.log(part);
+console.log(nums);
+const removed = nums.splice(2,5);
+console.log(removed);
+console.log(nums);
---
+++ scope.js
@@ -0,0 +1,11 @@
+function sum(first,second){
+    let bonus = 4;
+    let result = first+second+bonus;
+    if(result>9)
+    {
+        const mood = "happy";
+        console.log(mood);
+    }
+    return result;
+}
+const output = sum(3,8);
